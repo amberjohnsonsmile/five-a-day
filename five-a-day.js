92faa0a9ec5e5091b04a96a04ae5062ca85579be
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-10-18 19:09:32 -0600

    Initial commit

---
+++ five-a-day.js
@@ -0,0 +1,15 @@
+// Five-a-day fruit and vegetable counter using JavaScript and jQuery
+// Initial setup: whiteboard with counter on side
+
+$(document).ready(function() {
+  var total = 0;
+  $('#button').click(function() {
+    var toAdd = $('input[name=serving]').val();
+    $('.list').append('<li class="item">' + toAdd + '</li>');
+    total++;
+    $('#total-number').remove();
+    $('.total').append('<h3 id="total-number">' + total + '</h3>');
+  });
+});
+
+// Change mouse to "clickable" when hovering over button
